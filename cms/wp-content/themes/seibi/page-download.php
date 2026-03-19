ec625c2946ece479876b753c41f31297e874268a
Ryo Yoneda <yoneda@lep.jp>
2026-03-19 16:27:01 +0900

    wp:よくある質問ページ作成

--- cms/wp-content/themes/seibi/page-download.php
+++ cms/wp-content/themes/seibi/page-download.php
@@ -23,10 +23,16 @@ get_header(); ?>
 <section class="p-70-70">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-lg-8">
-        <?php if ( have_posts() ) : the_post(); ?>
-        <?php the_content(); ?>
-        <?php endif; ?>
+      <div class="col-md-6 col-12 mb-3 text-center">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/about/download-img/panph.webp" class="img-fluid mb-2" loading="lazy" alt="サレジアン国際学園小学校整備クラスパンフレット" />
+        <p>サレジアン国際学園小学校 パンフレット（PDFファイル4.7MB）</p>
+      </div>
+      <div class="col-12 mb-lg-5 text-center">
+        <a class="btn-slide btn-m btn-pink mb-3" href="<?php echo get_template_directory_uri(); ?>/img/about/download-img/panph2027.pdf" target="_blank">
+          <span class="text">パンフレットをダウンロードする<span class="material-symbols-outlined">open_in_new</span></span>
+        </a><br />
+        <small>リンクをクリックするとお使いのブラウザにカタログが表示されます。<br />
+          パンフレットを保存するにはお使いのブラウザから保存してください。</small>
       </div>
     </div>
   </div>
