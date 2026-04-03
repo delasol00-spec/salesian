1ffc940533b83b33631ea77e37f4cef8e6dad093
Ryo Yoneda <yoneda@lep.jp>
2026-04-03 14:43:17 +0900

    refactor: admin.php から卒業生アクセス制限を admin-graduate.php に分離

--- cms/wp-content/themes/seibi/functions.php
+++ cms/wp-content/themes/seibi/functions.php
@@ -10,4 +10,5 @@ require_once get_template_directory() . '/inc/enqueue.php';
 require_once get_template_directory() . '/inc/post-types.php';
 require_once get_template_directory() . '/inc/meta-boxes.php';
 require_once get_template_directory() . '/inc/admin.php';
+require_once get_template_directory() . '/inc/admin-graduate.php';
 require_once get_template_directory() . '/inc/auth.php';
