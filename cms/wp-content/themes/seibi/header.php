ba70be86b113a67f003c407ad776c01c61c1a77d
Ryo Yoneda <yoneda@lep.jp>
2026-04-16 10:01:00 +0900

    fix: GAタグをエンキューからheader.phpへ直接記述に変更

--- cms/wp-content/themes/seibi/header.php
+++ cms/wp-content/themes/seibi/header.php
@@ -1,6 +1,14 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-MW6F9J7550"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-MW6F9J7550');
+  </script>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.svg" type="image/svg+xml" />
